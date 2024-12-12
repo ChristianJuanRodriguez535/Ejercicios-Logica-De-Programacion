f974b068e80893915716204ae5c8f7a44c252c1c
ChristianJuanRodriguez535 <christianjuanrodriguez535@gmail.com>
2024-12-12 00:36:52 -0600

    Ejercicio 3

--- Ejercicio de Lógica de Programación 4/main.js
+++ Ejercicio de Lógica de Programación 4/main.js
@@ -1,11 +1,14 @@
 
     let num = prompt("Ingrese un número para generar la serie de Fibonacci ");
     
-    while (isNaN(num) || !Number.isInteger(parseFloat(num))) { alert("Por favor, ingrese un número válido.");
+    while (isNaN(num) || !Number.isInteger(parseFloat(num))) { 
+        
+        alert("Por favor, ingrese un número válido.");
 
         num = prompt ("Ingrese un número para generar la serie de Fibonacci ");
     }
     
+    num = parseInt(num);
 
 function serieFibonacci (num){
 
@@ -17,8 +20,9 @@ for (let i = 1; i < num; i++ ) {
     let  suma = num1 + num2;
     num1 = num2;
     num2 = suma
-   
-    serieFibonacci.push(suma);
+
+    //El bucle debe ejecutarse num - 1, ya que el cero ya esta incluido en la serie porque esta dentro del arreglo.
+    serieFibonacci.push(num1);
 
 }
   let resultado = serieFibonacci.join(', ');
